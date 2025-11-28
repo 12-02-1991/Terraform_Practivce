423f4699f1a9067615868fb51febee74f4f8519a
12-02-1991 <mnkgpt12@gmail.com>
2025-11-28 11:10:59 +0530

    added 1 AMI

--- Terraform_Practice/DataSource/main.tf
+++ Terraform_Practice/DataSource/main.tf
@@ -1,11 +1,17 @@
-data "aws_subnet" "name"{
+data "aws_subnet" "sub1"{
 filter {
   name="tag:Name" 
   values = ["Dev"]
 }
 }
+data "aws_subnet" "sub2"{
+filter {
+  name="tag:Name" 
+  values = ["Test"]
+}
+}
 
-data "aws_ami" "amzlinux" {
+data "aws_ami" "amzlinux1" {
   most_recent = true
   owners = [ "amazon" ]
   filter {
@@ -34,9 +40,37 @@ data "aws_ami" "amzlinux" {
 #   }
 
 # }
+
+data "aws_ami" "amzlinux2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.20251014.0-x86_64-gp2"]
+  }
+             filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+        filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
 resource "aws_instance" "name" {
-    ami=data.aws_ami.amzlinux.id
+    ami=data.aws_ami.amzlinux1.id
     instance_type = "t2.micro"
-    subnet_id = data.aws_subnet.name.id
+    subnet_id = data.aws_subnet.sub1.id
+
+}
+resource "aws_instance" "name" {
+    ami=data.aws_ami.amzlinux2.id
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.sub2.id
 
 }
